ce3a46028606ba4cf8d709a9705e862f19f8d5ac
SON-JISU <sjs961013@gmail.com>
2021-11-18 16:39:19 +0900

    step1

--- http.sh
+++ http.sh
@@ -1,6 +1,7 @@
-rm -rf /jen
-mkdir /jen
-chmod 777 /jen
-docker rm -f j1
-docker run -itd -p 60800:8080 -v /jen:/var/jenkins_home --name j1 jenkins/jenkins:lts
-docker exec j1 cat /var/jenkins_home/secrets/initialAdminPassword
+#!/bin/bash
+rm -rf /var/www/html/index.html
+yum remove -y httpd
+yum install -y httpd
+chmod 777 /var/www/html
+echo "Jenkins-github-Collaboration" >> /var/www/html/index.html
+systemctl start httpd
